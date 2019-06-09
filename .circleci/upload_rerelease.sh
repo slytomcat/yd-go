94f576659307e29dddde46c23091fc22b0b19807
Sly_tom_cat <slytomcat@mail.ru>
2019-06-09 19:39:46 +0300

    changed .circleci/upload_rerelease.sh

--- .circleci/upload_rerelease.sh
+++ .circleci/upload_rerelease.sh
@@ -2,11 +2,14 @@
 #
 
 # check that this is the release
-export TAG=$(git describe --abbrev=0 --tags); 
-#if [[ $TAG != $(git describe --tags) ]]; then 
-#  # exit if it is not release
-#  exit 0
-#fi
+export TAG=$(git describe --abbrev=0 --tags);
+echo Last release is $TAG
+CTAG=$(git describe --tags)
+echo Current tag is $CTAG  
+if [[ $TAG != $CTAG ]]; then 
+  # exit if it is not release
+  exit 0
+fi
 
 # get upload assets utility
 wget https://gist.githubusercontent.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447/raw/dbadd7d310ce8446de89c4ffdf1db0b400d0f6c3/upload-github-release-asset.sh
