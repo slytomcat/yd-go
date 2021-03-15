21d1b7e47e642fcbdec62261baaa00ad30ad4faf
slytomcat <slytomcat@mail.ru>
2021-03-16 00:47:21 +0300

    more tests

--- icons/icons_test.go
+++ icons/icons_test.go
@@ -2,8 +2,104 @@ package icons
 
 import "testing"
 
-func Test_Data01(t *testing.T) {
-	if len(darkBusy1) == 0 || len(darkBusy2) == 0 || len(darkBusy3) == 0 || len(darkBusy4) == 0 || len(darkBusy5) == 0 {
-		t.Error("len(darkBusy) = 0")
+func Test_Data_lightBusy1(t *testing.T) {
+	if len(lightBusy1) == 0 {
+		t.Error("len( lightBusy1) = 0")
+	}
+}
+
+func Test_Data_lightBusy2(t *testing.T) {
+	if len(lightBusy2) == 0 {
+		t.Error("len( lightBusy2) = 0")
+	}
+}
+
+func Test_Data_lightBusy3(t *testing.T) {
+	if len(lightBusy3) == 0 {
+		t.Error("len( lightBusy3) = 0")
+	}
+}
+
+func Test_Data_lightBusy4(t *testing.T) {
+	if len(lightBusy4) == 0 {
+		t.Error("len( lightBusy4) = 0")
+	}
+}
+
+func Test_Data_lightBusy5(t *testing.T) {
+	if len(lightBusy5) == 0 {
+		t.Error("len( lightBusy5) = 0")
+	}
+}
+
+func Test_Data_lightError(t *testing.T) {
+	if len(lightError) == 0 {
+		t.Error("len( lightError) = 0")
+	}
+}
+
+func Test_Data_lightIdle(t *testing.T) {
+	if len(lightIdle) == 0 {
+		t.Error("len( lightIdle) = 0")
+	}
+}
+
+func Test_Data_lightPause(t *testing.T) {
+	if len(lightPause) == 0 {
+		t.Error("len( lightPause) = 0")
+	}
+}
+
+func Test_Data_darkBusy1(t *testing.T) {
+	if len(darkBusy1) == 0 {
+		t.Error("len(darkBusy1) = 0")
+	}
+}
+
+func Test_Data_darkBusy2(t *testing.T) {
+	if len(darkBusy2) == 0 {
+		t.Error("len(darkBusy2) = 0")
+	}
+}
+
+func Test_Data_darkBusy3(t *testing.T) {
+	if len(darkBusy3) == 0 {
+		t.Error("len(darkBusy3) = 0")
+	}
+}
+
+func Test_Data_darkBusy4(t *testing.T) {
+	if len(darkBusy4) == 0 {
+		t.Error("len(darkBusy4) = 0")
+	}
+}
+
+func Test_Data_darkBusy5(t *testing.T) {
+	if len(darkBusy5) == 0 {
+		t.Error("len(darkBusy5) = 0")
+	}
+}
+
+func Test_Data_darkError(t *testing.T) {
+	if len(darkError) == 0 {
+		t.Error("len(darkError) = 0")
+	}
+}
+
+func Test_Data_darkIdle(t *testing.T) {
+	if len(darkIdle) == 0 {
+		t.Error("len(darkIdle) = 0")
+	}
+}
+
+func Test_Data_darkPause(t *testing.T) {
+	if len(darkPause) == 0 {
+		t.Error("len(darkPause) = 0")
+	}
+}
+
+func Test_Data_yd128(t *testing.T) {
+	if len(yd128) == 0 {
+		t.Error("len(yd128) = 0")
 	}
 }
