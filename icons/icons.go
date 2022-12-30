30097d9fbf1afc01b085421c8561f18877e15e12
Sly_tom_cat <slytomcat@mail.ru>
2022-12-30 18:06:47 +0300

    Correct icons handling

--- icons/icons.go
+++ icons/icons.go
@@ -1,8 +1,9 @@
 package icons
 
 import (
+	"context"
 	"os"
-	"sync/atomic"
+	"sync"
 	"time"
 
 	"github.com/slytomcat/llog"
@@ -12,16 +13,17 @@ var interval = time.Millisecond * 333
 
 // Icon is the icon helper
 type Icon struct {
-	NotifyIcon    string // path to notification icon stored as file on disk
+	NotifyIcon    string     // path to notification icon stored as file on disk
+	lock          sync.Mutex // data protection lock
 	currentStatus string
-	currentIcon   int64
+	currentIcon   int
 	busyIcons     [5][]byte
 	idleIcon      []byte
 	pauseIcon     []byte
 	errorIcon     []byte
 	setFunc       func([]byte)
 	ticker        *time.Ticker
-	exit          chan struct{}
+	stopper       func()
 }
 
 // NewIcon initializes the icon helper and retuns it.
@@ -36,23 +38,26 @@ func NewIcon(theme string, set func([]byte)) *Icon {
 		llog.Critical(err)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
 	i := &Icon{
 		currentStatus: "",
 		currentIcon:   0,
 		NotifyIcon:    file.Name(),
 		setFunc:       set,
 		ticker:        time.NewTicker(interval),
-		exit:          make(chan struct{}, 1),
+		stopper:       cancel,
 	}
 	i.ticker.Stop()
 	i.SetTheme(theme)
-	go i.loop()
+	go i.loop(ctx)
 	i.setFunc(i.pauseIcon)
 	return i
 }
 
 // SetTheme select one of the icons' themes
 func (i *Icon) SetTheme(theme string) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	switch theme {
 	case "light":
 		i.busyIcons = [5][]byte{lightBusy1, lightBusy2, lightBusy3, lightBusy4, lightBusy5}
@@ -75,7 +80,7 @@ func (i *Icon) SetTheme(theme string) {
 func (i *Icon) setIcon(status string) {
 	switch status {
 	case "busy", "index":
-		i.setFunc(i.busyIcons[atomic.LoadInt64(&i.currentIcon)])
+		i.setFunc(i.busyIcons[i.currentIcon])
 	case "idle":
 		i.setFunc(i.idleIcon)
 	case "none", "paused":
@@ -87,7 +92,8 @@ func (i *Icon) setIcon(status string) {
 
 // Set sets the icon by status
 func (i *Icon) Set(status string) {
-	i.setIcon(status)
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	if status == "busy" || status == "index" {
 		if i.currentStatus != "busy" && i.currentStatus != "index" {
 			i.ticker.Reset(interval)
@@ -96,15 +102,18 @@ func (i *Icon) Set(status string) {
 		i.ticker.Stop()
 	}
 	i.currentStatus = status
+	i.setIcon(status)
 }
 
-func (i *Icon) loop() {
+func (i *Icon) loop(ctx context.Context) {
 	for {
 		select {
 		case <-i.ticker.C:
-			atomic.StoreInt64(&i.currentIcon, (i.currentIcon+1)%5)
+			i.lock.Lock()
+			i.currentIcon = (i.currentIcon + 1) % 5
 			i.setFunc(i.busyIcons[i.currentIcon])
-		case <-i.exit:
+			i.lock.Unlock()
+		case <-ctx.Done():
 			return
 		}
 	}
@@ -113,7 +122,7 @@ func (i *Icon) loop() {
 // CleanUp removes temporary file for notification icon and stops internal loop
 func (i *Icon) CleanUp() {
 	i.ticker.Stop()
-	close(i.exit)
+	i.stopper()
 	if err := os.Remove(i.NotifyIcon); err != nil {
 		llog.Critical(err)
 	}
