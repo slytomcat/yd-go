4b968a533ebc05978726e1caf024485d15612af5
Pavel Statsenko <p.stacenko@yablochkovtech.com>
2024-11-02 18:59:19 +0400

    feat: added some additional checks

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
-### build script
 
-GO111MODULE=on CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags "-s -w -X main.version=v.$(git branch --show-current)-$(git rev-parse --short HEAD)"
+die() {
+    local message=$1
+    echo "$message" >&2
+    exit 1
+}
 
-if ! "$?"; then
-  echo "Build was successful!"
-else
-  echo "Sorry, build failed."
-fi
+GO111MODULE=on CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags "-s -w -X main.version=v.$(git branch --show-current)-$(git rev-parse --short HEAD)" || die 'Sorry, build just failed.'
 
 if type "upx" > /dev/null; then
   upx -qqq --best yd-go
 else
   echo "Executable is uncompressed due to 'upx' command was not found. You need to install it and re-run the script again."
 fi
+
