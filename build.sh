413630a2f0c594afe0a7e6c96746e417baaf3ad0
Pavel <prostopasta@users.noreply.github.com>
2024-11-02 18:28:44 +0400

    Update build.sh script
    Added 'GO111MODULE=on' variable to allow the download for any missing imported modules. 
    
    Also some additional checks: 
    - for build success or fail;
    - 'upx' compressor command check if available before using it.

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 ### build script
 
-CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags "-s -w -X main.version=v.$(git branch --show-current)-$(git rev-parse --short HEAD)"
-upx -qqq --best yd-go
+GO111MODULE=on CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags "-s -w -X main.version=v.$(git branch --show-current)-$(git rev-parse --short HEAD)"
 
+if ! "$?"; then
+  echo "Build was successful!"
+else
+  echo "Sorry, build failed."
+fi
+
+if type "upx" > /dev/null; then
+  upx -qqq --best yd-go
+else
+  echo "Executable is uncompressed due to 'upx' command was not found. You need to install it and re-run the script again."
+fi
