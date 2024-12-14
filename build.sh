32354f0ced55b4d36062cbf8207e87c16e1e177b
Sly_tom_cat <slytomcat@mail.ru>
2024-12-14 12:26:52 +0300

    Add upx check

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 ### build script
 
 CGO_ENABLED=0 go build -buildvcs=false -trimpath -ldflags "-s -w -X main.version=v.$(git branch --show-current)-$(git rev-parse --short HEAD)"
-upx -qqq --best yd-go
 
+if which upx >/dev/null 2>&1
+then
+   echo upx -qqq --best yd-go
+else
+   echo "'upx' is not installed and binary is not compressed. If You need compressed binary then install 'upx' and rebuild."
+fi
